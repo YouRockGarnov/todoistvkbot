b626a2cfa0709ff11ee2e42bf384dfb47636b358
Yury Garnov <hamta@yandex.ru>
2018-08-21 21:45:46 +0300

    AutorizedState is almost done! Added AskedState

--- states/Todoist/AutorizedState.py
+++ states/Todoist/AutorizedState.py
@@ -1,25 +1,63 @@
 from states.Bases.StateBase import StateBase
-
+from states.Bases.AskedState import AskedState
+import random
 
 class AutorizedState(StateBase):
+    clarifying_questions = [' Все верно?']
+
     def act(self, data, service):
+        message = data['object']['title']
 
-        # TODO добавить вопрос к пользователю о корректности добавления события
+        parsing = self.parse_message(message, service)
 
-        self._next_state = self
+        result = self._create_message(parsing)
+        if self._need_to_ask(parsing):
+            self._next_state = AskedState()
+            result += random.choice(AutorizedState.clarifying_questions)
+        else:
+            self._next_state = self
+
+        self._messages.append(result)
 
     def parse_message(self, message, service):
         response = {}
 
+        result = self._parse_project()
+        response['project'] = result['project']
+        message = result['edited_message']
 
+        # TODO добавь проверку есть ли в сообщении дата
 
-    def parse_project(self, message: str, service):
+        response['task'] = message
+        return response
+
+    def _parse_project(self, message: str, service):
         projects = service.get_project_names()
 
-        contexts = ['в {0}', 'В {0}']
+        contexts = ['в {0}', 'В {0}'] # строки, которые могут встретится,
+                                        # если пользователь имел ввиду добавить в конкретный проект
         for project in projects:
-            inside = [c.format(project) for c in contexts if c.format(project) in message]
+            inside = [(project, c.format(project)) for c in contexts if c.format(project) in message]
+                # понять есть ли контексты с различными проектами в сообщении
+
             if inside == []:
-                return {'exists': False, 'message': message}
+                return {'project': 'Входящие', 'edited_message': message} # edited_message - сообщение без проекта
             else:
-                return {'exists': True, 'message': message.replace(inside[0], '')}
+                return {'project': inside[0][0], 'edited_message': message.replace(inside[0][1], '')}
+
+    def _need_to_ask(self, parsing):
+        import datetime
+        return not (parsing['project'] == 'Входящие' and parsing['date'] == None)
+
+    def _create_message(self, parsing):
+        res_message = 'Я добавил {task} '.format(task=parsing['task'])
+        res_message += 'в проект {0} '.format(parsing['project'])
+
+        if parsing['date'] != None:
+            res_message += 'на {0} '.format(parsing['date'])
+
+        return res_message
+
+
+
+
