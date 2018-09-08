b6892a36689b63ceda57e169c5e2ab281c0a6a51
Yury Garnov <hamta@yandex.ru>
2018-09-08 12:30:13 +0300

    Added db things from sender_bot

--- TodoistVK.py
+++ TodoistVK.py
@@ -1,16 +1,53 @@
-from flask import Flask, request, json
+from flask import Flask, request, json, g
 from configs.config_vkbot import *
 from settings import *
-from tools.debug import *
+import tools.debug as debug_module
 from bots.vkbot import VKBot
 from tools.log import logger
 from app import app
+from db.creating_scratch import init_db, db_proxy
+import db.creating_scratch as creating_scratch
 
 bot = VKBot()
 
 @app.route('/', methods=['GET'])
 def describe():
     return 'Это TodoistVKBot!!!'
+
+@app.before_request
+def before_request():
+    init_db()
+    g.db = db_proxy
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+@app.route('/setDEBUG_True', methods=['GET'])
+def setDEBUG_True():
+    debug_module.setDEBUG(True)
+    return 'DEBUG = True'
+
+@app.route('/setDEBUG_False', methods=['GET'])
+def setDEBUG_False():
+    debug_module.setDEBUG(False)
+    return 'DEBUG = False'
+
+@app.route('/getDEBUG_Flag', methods=['GET'])
+def get_debug():
+    print(debug_module.getDEBUG())
+    return 'DEBUG = {0}'.format(debug_module.getDEBUG())
+
+
+@app.route('/create_db', methods=['GET'])
+def create_db():
+    return creating_scratch.create_db()
+
+@app.route('/reset_db', methods=['GET'])
+def reset_db():
+    return creating_scratch.reset_db()
 
 @app.route('/VK/Todoist', methods=['POST'])
 def processing():
@@ -19,7 +56,7 @@ def processing():
 
         logger.info('processing')
 
-        if DEBUG:
+        if debug_module.getDEBUG():
             logger.info('Run in debug')
 
         logger.info(request.data)
