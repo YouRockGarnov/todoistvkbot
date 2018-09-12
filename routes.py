8afcb8bccdfe0082929f7c51ccd6149f6e77d472
Yury Garnov <hamta@yandex.ru>
2018-09-12 10:20:58 +0300

    Deleted state_pull

--- routes.py
+++ routes.py
@@ -17,34 +17,31 @@ def todoist_redirect():
         }
     }
 
-    from services.TodoistService import TodoistService
-    logger.info(g.todoist_state_pull)
+    # TODO ADD CHECK EXISTANCE OF STATE (МОЖЕТ ЭТО ПОДДЕЛЬНЫЙ ЗАПРОС)
+
     logger.info(args)
-    if args['state'] in g.todoist_state_pull:
-        try:
-            sending_args = {'client_id': 'fb26051eb06649bb968791f3d7c2f185',
-                            'client_secret': '9d853dc5aba9490780682f159ff5c611',
-                            'code': args['code']}
+    try:
+        sending_args = {'client_id': 'fb26051eb06649bb968791f3d7c2f185',
+                        'client_secret': '9d853dc5aba9490780682f159ff5c611',
+                        'code': args['code']}
 
-            response = requests.post('https://todoist.com/oauth/access_token', sending_args)
+        response = requests.post('https://todoist.com/oauth/access_token', sending_args)
 
-            import json
-            str_access_token = json.loads(response.text)['access_token']
+        import json
+        str_access_token = json.loads(response.text)['access_token']
 
-            subs = Subscription.get(Subscription.messenger_user_id == user_id)
-            acc = subs.account
-            access_token = AccessToken(account=acc, token=str_access_token, service='Todoist')
-            access_token.save()
+        subs = Subscription.get(Subscription.messenger_user_id == user_id)
+        acc = subs.account
+        access_token = AccessToken(account=acc, token=str_access_token, service='Todoist')
+        access_token.save()
 
-        except Exception as ex:
-            import sys
-            logger.error(sys.exc_info())
-            data['object']['success'] = 'False'
-
-        global bot  # предполагается, что vkmain и telemain просто импортируют и там будут свои боты
-        bot.reply_to_message(data)  # шлем боту сообщение с флагом success
-    else:
+    except Exception as ex:
+        import sys
+        logger.error(sys.exc_info())
         data['object']['success'] = 'False'
+
+    global bot  # предполагается, что vkmain и telemain просто импортируют и там будут свои боты
+    bot.reply_to_message(data)  # шлем боту сообщение с флагом success
 
     return '<a href="javascript:close_window();">close</a>'
 
